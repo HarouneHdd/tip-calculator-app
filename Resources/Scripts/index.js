fd61c1f12dffdb12452431f8a78ef045fdf0b773
Haroune Hdd <75623924+HarouneHdd@users.noreply.github.com>
2021-11-20 11:50:21 +0100

    Completed, organized and optimized input validation

--- Resources/Scripts/index.js
+++ Resources/Scripts/index.js
@@ -8,74 +8,55 @@ const totalText = document.getElementById('total');
 
 const resetButton = document.getElementById('reset-button');
 
-let billValue = '';
-let tipValue = '';
-let peopleNumValue = '';
+let billPrevValue = '';
+let tipPrevValue = '';
+let peopleNumPrevValue = '';
 
 // This is the function that verifies against regex
 const checkAgainstRegex = (regex, str) => regex.test(str);
 
-billInput.addEventListener('input', () => {
-    const regex = new RegExp('(^[1-9][0-9]*$)|(^[1-9][0-9]*\.([0-9]{1,2})?$)');
-    const value = billInput.value;
+// Restrict what the user can input
+const validateInput = (inputType, curValue, prevValue) => {
+    let regex;
 
-    if (checkAgainstRegex(regex, value)) {
-        billValue = value;
+    if (inputType === 'integer') {
+        regex = new RegExp('(^0$)|(^[1-9][0-9]*$)');
+    }
+    else if (inputType === 'float') {
+        regex = new RegExp('(^0$)|(^0\\.([0-9]{1,2})?$)|(^[1-9][0-9]*$)|(^[1-9][0-9]*\\.([0-9]{1,2})?$)');
     }
     else {
-        if (value === '') {
-            billValue = '';
-        }
-
-        billInput.value = billValue;
+        console.error("Unidentified input type");
     }
-});
 
- // ---------------------------------------------------------- //
-/* I want to create one function that will validate the inputs
-   check for float and interger numbers */
-// ---------------------------------------------------------- //
-
-/* tipInput.addEventListener('input', () => {
-    const regex = new RegExp('(^0$)|(^[1-9][0-9]*$)');
-    const value = tipInput.value;
-
-    if (checkRegex(regex, value)) {
-        tipValue = value;
-    }
-    else {
-        if (value === '') {
-            tipValue = '';
-        }
-        else if (value !== '00' && value.length === 2 && checkRegex(regex, value[1])) {
-            // This will change 0 to a number from 1 to 9 depending on what the user
-            // entered, this so the user won't need to remove the number 0 to write 
-            // another
-            peopleNumValue = value[1];
-        }
-
-        peopleNumInput.value = peopleNumValue;
-    } 
-});*/
-
-peopleNumInput.addEventListener('input', () => {
-    const regex = new RegExp('(^0$)|(^[1-9][0-9]*$)');
-    const value = peopleNumInput.value;
-
-    if (checkAgainstRegex(regex, value)) {
-        peopleNumValue = value;
-    }
-    else {
-        if (value === '') {
-            peopleNumValue = '';
-        }
-        else if (value !== '00' && value.length === 2 && checkAgainstRegex(regex, value[1])) {
+    if (!checkAgainstRegex(regex, curValue)) {
+        
+        if (curValue !== '00' && curValue.length === 2 && checkAgainstRegex(regex, curValue[1])) {
             /* This will change 0 to a number from 1 to 9 depending on what the user
             entered, this so the user won't need to remove the number 0 to write 
             another*/
-            peopleNumValue = value[1];
+            prevValue = curValue[1];
         }
 
-        peopleNumInput.value = peopleNumValue;
+        if (curValue !== '') {
+            curValue = prevValue;
+        }
     }
+
+    return curValue;
+}
+
+billInput.addEventListener('input', () => {
+    billInput.value = validateInput('float', billInput.value, billPrevValue);
+    billPrevValue = billInput.value;
+});
+
+tipInput.addEventListener('input', () => {
+    tipInput.value = validateInput('integer', tipInput.value, tipPrevValue);
+    tipPrevValue = tipInput.value;
+});
+
+peopleNumInput.addEventListener('input', () => {
+    peopleNumInput.value = validateInput('integer', peopleNumInput.value, peopleNumPrevValue);
+    peopleNumPrevValue = peopleNumInput.value;
 });
